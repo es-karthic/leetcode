1c0690475bd57b1a95022357192a01ecb9cc2e68
printf <printf@Heaven.local>
2020-11-02 19:44:58 -0800

    insertion sort proper solution

--- src/main/java/com/leetcode/day2/Solution.java
+++ src/main/java/com/leetcode/day2/Solution.java
@@ -6,50 +6,22 @@ class Solution {
 
 
     public ListNode insertionSortList(ListNode head) {
-        // Initialize sorted linked list
-        ListNode sorted = null;
-        ListNode current = head;
-        // Traverse the given linked list and insert every
-        // node to sorted
-        while (current != null)
-        {
-            // Store next for next iteration
-            ListNode next = current.next;
-            // insert current in sorted linked list
-            sorted = sortedInsert(current,sorted);
-            // Update current
-            current = next;
+        if(head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        while(head != null){
+            ListNode temp = head.next;
+            if(prev.val >= head.val){
+                prev = dummy;
+            }
+            while(prev.next != null && prev.next.val < head.val){
+                prev = prev.next;
+            }
+            head.next = prev.next;
+            prev.next = head;
+            head = temp;
         }
-        // Update head_ref to point to sorted linked list
-        head = sorted;
-        return head;
+        return dummy.next;
     }
 
-    /*
-     * function to insert a new_node in a list. Note that
-     * this function expects a pointer to head_ref as this
-     * can modify the head of the input linked list
-     * (similar to push())
-     */
-    ListNode sortedInsert(ListNode newnode,ListNode sorted)
-    {
-        /* Special case for the head end */
-        if (sorted == null || sorted.val >= newnode.val)
-        {
-            newnode.next = sorted;
-            sorted = newnode;
-        }
-        else
-        {
-            ListNode current = sorted;
-            /* Locate the node before the point of insertion */
-            while (current.next != null && current.next.val < newnode.val)
-            {
-                current = current.next;
-            }
-            newnode.next = current.next;
-            current.next = newnode;
-        }
-        return sorted;
-    }
 }
