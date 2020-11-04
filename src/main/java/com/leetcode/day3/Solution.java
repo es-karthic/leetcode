7bb6ceaad43d78fc4af492b95aae140a09fda569
eskarthic <eskarthic@gmail.com>
2020-11-03 22:07:01 -0800

    correct soluiton for day3

--- src/main/java/com/leetcode/day3/Solution.java
+++ src/main/java/com/leetcode/day3/Solution.java
@@ -5,34 +5,25 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public int maxPower(String s) {
-        s = s+ " ";
-        Integer countValue = 0;
-        int prevCount = 0;
-        TreeMap<Character,Integer> count = new TreeMap<>();
-        char letters[] =s.toCharArray();
-        Character prev=letters[0];
-        for(Character letter:letters){
-            if(prev == letter){
-                countValue = countValue + 1;
-            } else {
-                if(count.containsKey(prev)){
-                    prevCount = count.get(prev);
-                    if(prevCount > countValue){
-                         countValue = prevCount;
-                    }
-                }
-                count.put(prev,countValue);
-                countValue = 1;
+        if(s.length() == 1)
+            return 1;
+
+        int max = 1;
+        int currMax = 1;
+
+        for(int i  = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                currMax++;
             }
-            prev = letter;
+            else{
+                currMax = 1;
+            }
+
+            if(currMax > max)
+                max = currMax;
         }
-        if(count.isEmpty()){
-            count.put(prev,countValue);
-        }
-        LinkedHashMap<Character,Integer> resultMap = count.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(e1,e2)->e1,LinkedHashMap::new));
-        Object[] keys = resultMap.keySet().toArray();
-        int len = keys.length>0?keys.length-1:0;
-        Character letter = (Character)keys[len];
-        return resultMap.get(letter);
+
+        return max;
+
     }
 }
